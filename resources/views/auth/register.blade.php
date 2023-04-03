4b2139b1a5bd121faf3f08ab6be58d1bcd432f96
Bahri Meriç Canlı <bahri@bahri.info>
2023-04-03 18:20:52 +0300

    disabled

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ trans("auth.register_title") }}</div>
 
@@ -113,7 +113,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-lg btn-primary">
+                                <button type="submit" class="btn btn-lg btn-primary" disabled="">
                                     {{ trans("auth.register") }}
                                 </button>
                             </div>
