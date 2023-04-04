b07714191721514281af99f339e5bfc143ee4e85
Bahri Meriç Canlı <bahri@bahri.info>
2023-04-04 11:30:09 +0300

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-4" style="background-color: #f8f8f8;">
             @yield('content')
         </main>
     </div>
