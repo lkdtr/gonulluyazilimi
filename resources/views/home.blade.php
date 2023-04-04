4ca2d9403289611b666802fb72241d177ae7dcc2
Bahri Meriç Canlı <bahri@bahri.info>
2023-04-05 00:23:55 +0300

    event

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,19 +15,30 @@
                     @endif
 
                     @foreach ($events as $event)
-                        <h3>{{$event->event_name}} ({{date("d-m-Y", strtotime($event->started_at))}} / {{date("d-m-Y", strtotime($event->finished_at))}} )</h3>
-                        <div>{!! $event->event_detail !!}</div>
-                        <p>Etkinlik konumu: {{$event->event_location}}</p>
-                        <hr>
-                        <form method="POST">
-                            @csrf
-                            <input type="hidden" name="event_id" value="{{$event->id}}">
-                            @if(isset($joined_events[$event->id]))
-                            <span class="btn btn-lg btn-primary disabled">Etklinğine kaydınız daha önce alınmış</span>
-                            @else
-                            <button class="btn btn-lg btn-primary" type="submit">Katın</button>
-                            @endif
-                        </form>
+                        <button style="width: 100%;text-align: left;" class="btn btn-lg btn-secondary" type="button" data-bs-toggle="collapse" href="#eventCollapse{{$event->id}}" role="button" aria-expanded="false" aria-controls="eventCollapse{{$event->id}}">
+                            {{$event->event_name}} ({{date("d-m-Y", strtotime($event->started_at))}} / {{date("d-m-Y", strtotime($event->finished_at))}} )
+                        </button>
+
+                        <div class="collapse" id="eventCollapse{{$event->id}}">
+                            <div class="card card-body text-dark bg-light">
+
+                                <form method="POST">
+                                    @csrf
+                                    <input type="hidden" name="event_id" value="{{$event->id}}">
+                                    @if(isset($joined_events[$event->id]))
+                                    <span class="btn btn-lg btn-primary disabled">Etklinğine kaydınız daha önce alınmış</span>
+                                    @else
+                                    <button class="btn btn-lg btn-primary" type="submit">Katın</button>
+                                    @endif
+                                </form>
+
+                                <hr>
+
+                                <div>{!! $event->event_detail !!}</div>
+                                <p>Etkinlik konumu: {{$event->event_location}}</p>
+
+                            </div>
+                        </div>
                     @endforeach
                 </div>
             </div>
