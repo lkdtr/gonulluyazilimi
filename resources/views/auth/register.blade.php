5db3394422fc067f78ec7fde8b058f0454d754ff
Bahri Meriç Canlı <bahri@bahri.info>
2023-04-05 22:26:17 +0300

    kayit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -57,7 +57,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ trans("auth.email") }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
