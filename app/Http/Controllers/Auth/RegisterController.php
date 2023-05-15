73a40a9b90e4616bc33fb646018220ef3bc04591
Bahri Meriç Canlı <bahri@bahri.info>
2023-05-15 15:47:17 +0300

    tc kimlik doğrulama

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,8 +52,8 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
-            'national_id' => ['required', 'string', 'max:11'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'national_id' => ['required', 'string', 'max:11', 'unique:users,national_id', 'tckimlik'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
             'phone_number' => ['required', 'string', 'max:15'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'agreement' => ['required'],
