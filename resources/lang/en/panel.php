569a66b3df4dc37cf792ab914d2dc382bf4cc2f6
Bahri Meriç Canlı <bahri@bahri.info>
2023-05-15 16:16:15 +0300

    announcements

--- resources/lang/en/panel.php
+++ resources/lang/en/panel.php
@@ -3,6 +3,7 @@
 return [
 
     'available_events_title' => 'Available Events',
+    'available_announcements_title' => 'Available Announcements',
     "users_title" => 'Users',
     'users' => 'Users',
 
