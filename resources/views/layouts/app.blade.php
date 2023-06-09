97e7f4942d2db18b2b0558334a22f7f428587f9a
Bahri Meriç Canlı <bahri@bahri.info>
2023-06-09 17:03:27 +0300

    border

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,6 +115,7 @@
                                     <a class="dropdown-item" href="{{ route('my-infos') }}">
                                         {{trans('panel.my_infos')}}
                                     </a>
+                                    <hr style="margin: 5px; color: #999;">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
