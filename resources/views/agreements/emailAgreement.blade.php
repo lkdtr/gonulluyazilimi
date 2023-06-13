99ed859796df8c589566086b7db31d27960044ef
Bahri Meriç Canlı <bahri@bahri.info>
2023-06-13 15:36:12 +0300

    hizzalama

--- resources/views/agreements/emailAgreement.blade.php
+++ resources/views/agreements/emailAgreement.blade.php
@@ -1,24 +1,26 @@
-<p>Cumhurbaşkanlığı Dijital D&ouml;n&uuml;ş&uuml;m Bilgi ve İletişim G&uuml;venliği Tedbirleri ve Genelgesi, 6698 Sayılı Kişisel Verilerin Korunması Kanunu ve ISO 27001:2013 Bilgi G&uuml;venliği Y&ouml;netim sisteminde belirtilen maddelere dayanarak Kişi;</p>
+<div style="line-height: 30px; text-align: justify;">
+    <p>Cumhurbaşkanlığı Dijital D&ouml;n&uuml;ş&uuml;m Bilgi ve İletişim G&uuml;venliği Tedbirleri ve Genelgesi, 6698 Sayılı Kişisel Verilerin Korunması Kanunu ve ISO 27001:2013 Bilgi G&uuml;venliği Y&ouml;netim sisteminde belirtilen maddelere dayanarak Kişi;</p>
 
-<ol>
-	<li>Linux Kullanıcıları Derneği e-posta servisini kullandığında ileri s&uuml;rd&uuml;ğ&uuml; şahsi fikir, d&uuml;ş&uuml;nce ve ifadelerin, Linux Kullanıcıları Derneği ortamına eklediği dosyaların sorumluluğunun kendisine ait olduğunu ve Linux Kullanıcıları Derneği&#39;nin bu dosyalardan dolayı hi&ccedil;bir şekilde sorumlu tutulamayacağını,</li>
-	<li>Linux Kullanıcıları Derneği e-posta hizmetlerinde, site geneline zarar verecek veya Linux Kullanıcıları Derneği&rsquo;ni başka kişi ya da kuruluşlarla mahkemelik duruma getirecek herhangi bir yazılım veya materyal bulunduramayacağını, paylaşamayacağını ve cezai bir durum doğarsa t&uuml;m cezai sorumlulukları &uuml;st&uuml;ne aldığını,</li>
-	<li>E-posta servisinin kullanımı sırasında kaybolacak ve/veya eksik alınacak, yanlış adrese iletilecek bilgi, mesaj ve dosyalardan Linux Kullanıcıları Derneği&#39;nin sorumlu olmayacağını,</li>
-	<li>E-posta adresi verilerinin Linux Kullanıcıları Derneği&#39;nin ihmali g&ouml;r&uuml;lmeden yetkisiz kişilerce okunmasından (kişinin bilgilerini başka kişiler ile paylaşması, siteden ayrılırken &ccedil;ıkış yapmaması, vb. durumlardan) dolayı gelebilecek zararlardan &ouml;t&uuml;r&uuml; Linux Kullanıcıları Derneği&#39;nin sorumlu olmayacağını,</li>
-	<li>Tehdit edici, ahlak dışı, ırk&ccedil;ı, ayrımcı, T&uuml;rkiye Cumhuriyeti Yasalarına, vatandaşı olduğu diğer &uuml;lkelerin yasalarına ve uluslararası anlaşmalara aykırı mesajlar g&ouml;ndermemeyi,</li>
-	<li>Ortama eklenecek yazışmaların, konu başlıklarının, genel ahlak, g&ouml;rg&uuml; ve hukuk kurallarına uygun olmasını,</li>
-	<li>Diğer kullanıcıları taciz ve tehdit etmemeyi,</li>
-	<li>Diğer kullanıcıların servisi kullanmasını etkileyecek şekilde davranmamayı,</li>
-	<li>Diğer kullanıcıların bilgisayarındaki bilgilere ya da yazılıma zarar verecek bilgi veya programlar g&ouml;ndermemeyi,</li>
-	<li>Linux Kullanıcıları Derneği e-posta servisini kullanarak elde edilen herhangi bir kayıt veya elde edilmiş malzemelerin tamamıyla kullanıcının rızası d&acirc;hilinde olduğunu, kullanıcı bilgisayarında yaratacağı arızalar, bilgi kaybı ve diğer kayıpların sorumluluğunun tamamıyla kendisine ait olduğunu, servisin kullanımından dolayı uğrayabileceği zararlardan dolayı Linux Kullanıcıları Derneği&rsquo;nden tazminat talep etmemeyi,</li>
-	<li>Kurallara aykırı davrandığı takdirde Linux Kullanıcıları Derneği&#39;nin gerekli m&uuml;dahalelerde bulunma, kişiyi hizmet dışına &ccedil;ıkarma ve &uuml;yeliğine son verme hakkına sahip olduğunu,</li>
-	<li>Kanunlara g&ouml;re postalanması yasak olan bilgileri postalamamayı ve zincir posta(chain mail), yazılım vir&uuml;s&uuml; (vb.) gibi g&ouml;nderilme yetkisi olmayan postaları dağıtmamayı,</li>
-	<li>E-posta &quot;kullanıcı&quot; adıyla yapacağı her t&uuml;rl&uuml; işlemden bizzat kendisinin sorumlu olduğunu,</li>
-	<li>E-posta hesap şifresini d&uuml;zenli aralıklarla (yılda bir defa) yenileyeceğini (sistem otomatik olarak kullanıcıları şifre yenilemeye zorlayacaktır) ve bu yenilenecek şifrelerde, kişisel bilgilerle ilişkili ve kolay tahmin edilecek rakam ya da harf dizileri yerine b&uuml;y&uuml;k k&uuml;&ccedil;&uuml;k harf, rakam ve sembollerden oluşacak kombinasyonları tercih edeceğini,</li>
-	<li>Sahip olduğu e-posta hesabı/hesapları &uuml;zerinden yetkili/sorumlu olduğu iş ve g&ouml;revler &ccedil;er&ccedil;evesinde ilgili sistemlere erişim sağlayacağını,</li>
-	<li>E-posta hesabını tek taraflı olarak iptal ettirse bile, bu iptal işleminden &ouml;nce, &uuml;yeliği sırasında ger&ccedil;ekleştirdiği icraatlardan kendisinin sorumlu olacağını,</li>
-</ol>
+    <ol>
+        <li>Linux Kullanıcıları Derneği e-posta servisini kullandığında ileri s&uuml;rd&uuml;ğ&uuml; şahsi fikir, d&uuml;ş&uuml;nce ve ifadelerin, Linux Kullanıcıları Derneği ortamına eklediği dosyaların sorumluluğunun kendisine ait olduğunu ve Linux Kullanıcıları Derneği&#39;nin bu dosyalardan dolayı hi&ccedil;bir şekilde sorumlu tutulamayacağını,</li>
+        <li>Linux Kullanıcıları Derneği e-posta hizmetlerinde, site geneline zarar verecek veya Linux Kullanıcıları Derneği&rsquo;ni başka kişi ya da kuruluşlarla mahkemelik duruma getirecek herhangi bir yazılım veya materyal bulunduramayacağını, paylaşamayacağını ve cezai bir durum doğarsa t&uuml;m cezai sorumlulukları &uuml;st&uuml;ne aldığını,</li>
+        <li>E-posta servisinin kullanımı sırasında kaybolacak ve/veya eksik alınacak, yanlış adrese iletilecek bilgi, mesaj ve dosyalardan Linux Kullanıcıları Derneği&#39;nin sorumlu olmayacağını,</li>
+        <li>E-posta adresi verilerinin Linux Kullanıcıları Derneği&#39;nin ihmali g&ouml;r&uuml;lmeden yetkisiz kişilerce okunmasından (kişinin bilgilerini başka kişiler ile paylaşması, siteden ayrılırken &ccedil;ıkış yapmaması, vb. durumlardan) dolayı gelebilecek zararlardan &ouml;t&uuml;r&uuml; Linux Kullanıcıları Derneği&#39;nin sorumlu olmayacağını,</li>
+        <li>Tehdit edici, ahlak dışı, ırk&ccedil;ı, ayrımcı, T&uuml;rkiye Cumhuriyeti Yasalarına, vatandaşı olduğu diğer &uuml;lkelerin yasalarına ve uluslararası anlaşmalara aykırı mesajlar g&ouml;ndermemeyi,</li>
+        <li>Ortama eklenecek yazışmaların, konu başlıklarının, genel ahlak, g&ouml;rg&uuml; ve hukuk kurallarına uygun olmasını,</li>
+        <li>Diğer kullanıcıları taciz ve tehdit etmemeyi,</li>
+        <li>Diğer kullanıcıların servisi kullanmasını etkileyecek şekilde davranmamayı,</li>
+        <li>Diğer kullanıcıların bilgisayarındaki bilgilere ya da yazılıma zarar verecek bilgi veya programlar g&ouml;ndermemeyi,</li>
+        <li>Linux Kullanıcıları Derneği e-posta servisini kullanarak elde edilen herhangi bir kayıt veya elde edilmiş malzemelerin tamamıyla kullanıcının rızası d&acirc;hilinde olduğunu, kullanıcı bilgisayarında yaratacağı arızalar, bilgi kaybı ve diğer kayıpların sorumluluğunun tamamıyla kendisine ait olduğunu, servisin kullanımından dolayı uğrayabileceği zararlardan dolayı Linux Kullanıcıları Derneği&rsquo;nden tazminat talep etmemeyi,</li>
+        <li>Kurallara aykırı davrandığı takdirde Linux Kullanıcıları Derneği&#39;nin gerekli m&uuml;dahalelerde bulunma, kişiyi hizmet dışına &ccedil;ıkarma ve &uuml;yeliğine son verme hakkına sahip olduğunu,</li>
+        <li>Kanunlara g&ouml;re postalanması yasak olan bilgileri postalamamayı ve zincir posta(chain mail), yazılım vir&uuml;s&uuml; (vb.) gibi g&ouml;nderilme yetkisi olmayan postaları dağıtmamayı,</li>
+        <li>E-posta &quot;kullanıcı&quot; adıyla yapacağı her t&uuml;rl&uuml; işlemden bizzat kendisinin sorumlu olduğunu,</li>
+        <li>E-posta hesap şifresini d&uuml;zenli aralıklarla (yılda bir defa) yenileyeceğini (sistem otomatik olarak kullanıcıları şifre yenilemeye zorlayacaktır) ve bu yenilenecek şifrelerde, kişisel bilgilerle ilişkili ve kolay tahmin edilecek rakam ya da harf dizileri yerine b&uuml;y&uuml;k k&uuml;&ccedil;&uuml;k harf, rakam ve sembollerden oluşacak kombinasyonları tercih edeceğini,</li>
+        <li>Sahip olduğu e-posta hesabı/hesapları &uuml;zerinden yetkili/sorumlu olduğu iş ve g&ouml;revler &ccedil;er&ccedil;evesinde ilgili sistemlere erişim sağlayacağını,</li>
+        <li>E-posta hesabını tek taraflı olarak iptal ettirse bile, bu iptal işleminden &ouml;nce, &uuml;yeliği sırasında ger&ccedil;ekleştirdiği icraatlardan kendisinin sorumlu olacağını,</li>
+    </ol>
 
-<p>T&uuml;m bu maddeleri daha sonra hi&ccedil;bir itiraza mahal vermeyecek şekilde okuyup anladığını,</p>
+    <p>T&uuml;m bu maddeleri daha sonra hi&ccedil;bir itiraza mahal vermeyecek şekilde okuyup anladığını,</p>
 
-<p>KABUL VE TAAHH&Uuml;T ETMİŞTİR.</p>
+    <p>KABUL VE TAAHH&Uuml;T ETMİŞTİR.</p>
+</div>
