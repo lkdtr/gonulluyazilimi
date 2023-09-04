2a33c2686fb186d2429d8caf0b3db8d1b7a2cc95
Bahri Meriç Canlı <bahri@bahri.info>
2023-09-04 13:20:46 +0300

    başlık

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card border-secondary">
-                <div class="card-header text-white bg-secondary">Linux Kullanıcıları Derneği Gönüllü Sistemi Nedir?</div>
+                <div class="card-header text-white bg-secondary">Linux Kullanıcıları Derneği Gönüllüsü Nedir?</div>
 
                 <div class="card-body">
                     @if (session('status'))
