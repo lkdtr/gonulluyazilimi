1e44141a7321e0b2dddf9864c6aaffe89f8ca32e
Bahri Meriç Canlı <bahri@bahri.info>
2026-08-25 17:19:47 +0300

    fix: use writable sanitizer cache

--- app/Support/HtmlSanitizer.php
+++ app/Support/HtmlSanitizer.php
@@ -4,12 +4,17 @@ namespace App\Support;
 
 use HTMLPurifier;
 use HTMLPurifier_Config;
+use Illuminate\Support\Facades\File;
 
 class HtmlSanitizer
 {
     public function sanitize(?string $html): string
     {
         $config = HTMLPurifier_Config::createDefault();
+        $cachePath = storage_path('app/htmlpurifier');
+
+        File::ensureDirectoryExists($cachePath);
+        $config->set('Cache.SerializerPath', $cachePath);
         $config->set('HTML.Allowed', 'p,br,b,strong,i,em,u,ul,ol,li,h1,h2,h3,h4,h5,h6,blockquote,pre,code,a[href|title|target|rel],table,thead,tbody,tr,th,td');
         $config->set('Attr.AllowedFrameTargets', ['_blank']);
         $config->set('HTML.Nofollow', true);
